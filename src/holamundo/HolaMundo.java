eddb78e0c48e98ed7f6f9b1c2ce82f605ede9629
asotodominguez <asotodominguez@victoria16.danielcastelao.org>
2017-10-06 09:39:10 +0200

    Inicio proyecto

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -4,7 +4,7 @@ package holamundo;
 public class HolaMundo {
 
     public static void main(String[] args) {
-        
+        //Texto en pantalla
         System.out.println("Hola mundo!");
     }
     
